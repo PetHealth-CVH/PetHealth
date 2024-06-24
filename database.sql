7a12bb28af2c3314a0b1ac18f81dea17dd3b6f1b
Andrey Leone <andreeyleonne92@gmail.com>
2024-06-24 17:51:05 -0300

    Recriando tabela de contato

--- database.sql
+++ database.sql
@@ -1,13 +1,6 @@
 -- Criei aqui o banco de dados DDL do projeto
-create database LojaPet;
-create table tb_credenciais (
-    id_credencial serial primary key,
-    id_usuario serial unique,
-    email varchar not null,
-    senha varchar not null,
 
-    foreign key (id_usuario) references tb_usuário(id_usuario)
-);
+create database LojaPet;
 
 create table tb_contato (
     id_contato serial primary key
@@ -15,8 +8,9 @@ create table tb_contato (
     email int not null
 
     foreign key (email) references tb_credenciais(email)
-);
-     create table tb_usuarios (
+    )
+
+create table tb_usuarios (
     id_usuario serial primary key,
     credencial serial,
     nome varchar(50) not null,
@@ -27,5 +21,16 @@ create table tb_contato (
     endereço serial,
 
     -- Aqui ficam as referências desta tabela com outras (FK)
-    foreign key (credencial) references tb_credenciais (id_credencial)
+    foreign key (credencial) references tb_credenciais (id),
+    foreign key (endereco) references tb_enderecos (id),
+    foreign key (contato) references tb_contatos (id)
+)
+
+create table tb_credenciais (
+    id_credencial serial primary key,
+    id_usuario serial unique,
+    email varchar not null,
+    senha varchar not null,
+
+    foreign key (id_usuario) references tb_usuário(id_usuario)
 )
