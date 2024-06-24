7fe7ec3a5625a2579fd3a5e806eb6fdf030ef0bc
Andrey Leone <andreeyleonne92@gmail.com>
2024-06-24 16:44:35 -0300

    adicionando

--- database.sql
+++ database.sql
@@ -1,9 +1,35 @@
 -- Criei aqui o banco de dados DDL do projeto
-
 create database LojaPet;
+create table tb_credenciais (
+    id_credencial serial primary key,
+    id_usuario serial unique,
+    email varchar not null,
+    senha varchar not null,
 
-create table tb_usuários (
+    foreign key (id_usuario) references tb_usuário(id_usuario)
+);
+
+create table tb_contato (
+    id_contato serial primary key
+    celular varchar (14) unique not null
+    email int not null
+
+    foreign key (email) references tb_credenciais(email)
+);
+
+create table tb_usuarios (
     id_usuario serial primary key,
+
+    
+        
+          
+    
+
+        
+        Expand All
+    
+    @@ -22,7 +30,7 @@ create table tb_usuarios (
+  
     credencial serial,
     nome varchar(50) not null,
     sobrenome varchar(50) not null,
@@ -13,16 +39,5 @@ create table tb_usuários (
     endereço serial,
 
     -- Aqui ficam as referências desta tabela com outras (FK)
-    foreign key (credencial) references tb_credenciais (id),
-    foreign key (endereço) references tb_endereços (id),
-    foreign key (contato) references tb_contatos (id)
-)
-
-create table tb_credenciais (
-    id_credencial serial primary key,
-    id_usuario serial unique,
-    email varchar not null,
-    senha varchar not null,
-
-    foreign key (id_usuario) references tb_usuário(id_usuario)
+    foreign key (credencial) references tb_credenciais (id_credencial)
 )
