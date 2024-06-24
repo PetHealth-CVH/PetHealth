6d915ad97c54fdfe8a0b5b5626a3ffc9c5af7627
Tiallysson Costa <tiallysson@proton.me>
2024-06-24 16:21:52 -0300

    Adicionado o arquivo databese.sql e as primeiras tabelas

---
+++ database.sql
@@ -0,0 +1,28 @@
+-- Criei aqui o banco de dados DDL do projeto
+
+create database LojaPet;
+
+create table tb_usuários (
+    id_usuario serial primary key,
+    credencial serial,
+    nome varchar(50) not null,
+    sobrenome varchar(50) not null,
+    cpf varchar(11) unique not null,
+    data_cadastro timestamp defaut NOW,
+    contato serial,
+    endereço serial,
+
+    -- Aqui ficam as referências desta tabela com outras (FK)
+    foreign key (credencial) references tb_credenciais (id),
+    foreign key (endereço) references tb_endereços (id),
+    foreign key (contato) references tb_contatos (id)
+)
+
+create table tb_credenciais (
+    id_credencial serial primary key,
+    id_usuario serial unique,
+    email varchar not null,
+    senha varchar not null,
+
+    foreign key (id_usuario) references tb_usuário(id_usuario)
+)
