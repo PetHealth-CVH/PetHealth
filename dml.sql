f435312c52bf28a920aecdda295a40b1e8cd8d8d
Ana Julia Mantovani <anajuliamantovani001@gmail.com>
2024-06-28 18:15:15 -0300

    Atualização da tabela

---
+++ dml.sql
@@ -0,0 +1,27 @@
+insert into tb_produtos(nome_do_produto, descricao, preco, quantidade)
+values 
+(
+    'Vermífugo Vermivet Composto 600mg para cães', 
+    'Vermifugo para tratamento e controle de verminoses para cães', 
+    9.90, 100
+), 
+
+(
+    'Antipulgas e Carrapatos para cães de 30 a 60kg', 
+    'Proteção contra pulgas, carrapatos, sarna e vermes',
+    69.90, 100
+),
+
+(
+    'Vermífugo Vetmax Plus 700mg para cães e gatos',
+    'Indicado no tratamento de vermes gastrointestinais em cães e gatos',
+    19.90, 100
+)
+
+(
+    'Suplemento Nutrafases Ômega 3 para Cães – 60 Tabletes',
+    'Suplemento alimentar, que contém Nano Ômega 3 e o ômega 3 micronizado, extraido
+    do óleo de peixe de águas frias, ricos em DHA e EPA e vitamina A;
+    Ideal para cães de qualquer idade',
+    53.60, 100
+)
