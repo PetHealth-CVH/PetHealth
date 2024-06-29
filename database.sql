01879a85eeb455b0a6f53990d5e34612ce8009f5
Wagner <wagnerluan72@gmail.com>
2024-06-29 13:10:16 -0300

    Adicionei registros

--- database.sql
+++ database.sql
@@ -2,35 +2,6 @@
 
 create database LojaPet;
 
-create table tb_enderecos (
-    id_endereco serial primary key,
-    cep varchar(9) not null,
-    rua varchar(100) not null,
-    bairro varchar(100), not null,
-    cidade varchar(50) not null,
-    estado varchar(50) not null,
-    numero varchar(10),
-    complemento varchar
-);
-
-create table tb_pedidos (
-    id_pedidos serial primary key,
-    id_usuario serial unique,
-    data_de_pedido timestamp defaut NOW,
-    produtos int not null,
-    quantidade int(100),
-    preco numeric(5,2) not null
-);
-
-create table tb_produtos (
-    id_produtos serial primary key,
-    nome_do_produto varchar not null,
-    descrição varchar not null,
-    quantidade int(100),
-    preco numeric(5,2) not null
-
-);
-
 create table tb_usuarios (
     id_usuario serial primary key,
     nome varchar(50) not null,
@@ -54,6 +25,17 @@ create table tb_credenciais (
     foreign key (id_usuario) references tb_usuarios (id_usuario)
 );
 
+create table tb_enderecos (
+    id_endereco serial primary key,
+    cep varchar(9) not null,
+    rua varchar(100) not null,
+    bairro varchar(100), not null,
+    cidade varchar(50) not null,
+    estado varchar(50) not null,
+    numero varchar(10),
+    complemento varchar
+);
+
 create table tb_contato (
     id_contato serial primary key,
     celular varchar(14) unique not null,
@@ -61,3 +43,22 @@ create table tb_contato (
 
     foreign key (email) references tb_credenciais (email)
 );
+
+create table tb_produtos (
+    id_produtos serial primary key,
+    nome_do_produto varchar not null,
+    descrição varchar not null,
+    quantidade int(100),
+    preco numeric(5,2) not null
+
+);
+
+create table tb_pedidos (
+    id_pedidos serial primary key,
+    id_usuario serial unique,
+    data_de_pedido timestamp defaut NOW,
+    produtos int not null,
+    quantidade int(100),
+    preco numeric(5,2) not null
+);
+
