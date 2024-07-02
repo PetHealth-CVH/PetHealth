322c82ee43d529723a15c27452c208f7ae0e7b14
Tiallysson Costa <tiallysson@proton.me>
2024-07-02 17:43:11 -0300

    Alterei a ordem de algumas tabelas no código

--- database.sql
+++ database.sql
@@ -2,29 +2,6 @@
 
 create database LojaPet;
 
-create table tb_usuarios (
-    id_usuario serial primary key,
-    nome varchar(50) not null,
-    sobrenome varchar(50) not null,
-    cpf varchar(11) unique not null,
-    data_cadastro timestamp defaut NOW,
-    id_contato int,
-    id_endereco int,
-
-    foreign key (id_contato) references tb_contato (id_contato),
-    foreign key (id_endereco) references tb_enderecos (id_endereco),
-    foreign key (id_usuario) references tb_pedidos (id_Pedidos)
-);
-
-create table tb_credenciais (
-    id_credencial serial primary key,
-    id_usuario serial unique,
-    email varchar not null,
-    senha varchar not null,
-
-    foreign key (id_usuario) references tb_usuarios (id_usuario)
-);
-
 create table tb_enderecos (
     id_endereco serial primary key,
     cep varchar(9) not null,
@@ -35,15 +12,6 @@ create table tb_enderecos (
     numero varchar(10),
     complemento varchar
 );
-
-create table tb_contato (
-    id_contato serial primary key,
-    celular varchar(14) unique not null,
-    email int not null,
-
-    foreign key (email) references tb_credenciais (email)
-);
-
 
 create table tb_produtos (
     id_produtos serial primary key,
@@ -63,4 +31,33 @@ create table tb_pedidos (
     preco numeric(5,2) not null
 );
 
- b5b899427190f84c32386a545f022280b010a872
+create table tb_usuarios (
+    id_usuario serial primary key,
+    nome varchar(50) not null,
+    sobrenome varchar(50) not null,
+    cpf varchar(11) unique not null,
+    data_cadastro timestamp defaut NOW,
+    id_contato int,
+    id_endereco int,
+
+    foreign key (id_contato) references tb_contato (id_contato),
+    foreign key (id_endereco) references tb_enderecos (id_endereco),
+    foreign key (id_usuario) references tb_pedidos (id_Pedidos)
+);
+
+create table tb_credenciais (
+    id_credencial serial primary key,
+    id_usuario serial unique,
+    email varchar unique not null,
+    senha varchar not null,
+
+    foreign key (id_usuario) references tb_usuarios (id_usuario)
+);
+
+create table tb_contato (
+    id_contato serial primary key,
+    celular varchar(14) unique not null,
+    email int not null,
+
+    foreign key (email) references tb_credenciais (email)
+);
