8cf6f7f63fb62cd1bae4590539a33afd00bb60d3
Tiallysson Costa <tiallysson@proton.me>
2024-07-03 16:49:58 -0300

    Adicionei uma foreing key na `tb_pedidos`

--- database.sql
+++ database.sql
@@ -17,19 +17,16 @@ create table tb_enderecos (
 
 create table tb_produtos (
     id_produtos serial primary key,
-    nome_produto varchar not null,
-    descricao varchar not null,
-    quantidade int(100),
+    nome_produto varchar(100) not null,
+    descricao varchar(255) not null,
+    quantidade int not null,
     preco numeric(5,2) not null
 );
 
-create table tb_pedidos (
-    id_pedidos serial primary key,
-    id_usuario serial unique,
-    data_pedido timestamp defaut NOW,
-    produtos int not null,
-    quantidade int(100),
-    preco numeric(5,2) not null
+create table tb_contato (
+    id_contato serial primary key,
+    celular varchar(14) unique not null,
+    email varchar(100) unique,
 );
 
 create table tb_usuarios (
@@ -37,19 +34,12 @@ create table tb_usuarios (
     nome varchar(50) not null,
     sobrenome varchar(50) not null,
     cpf varchar(11) unique not null,
-    data_cadastro timestamp defaut NOW,
+    data_cadastro timestamp defaut NOW(),
     id_contato int,
     id_endereco int,
 
     foreign key (id_contato) references tb_contato (id_contato),
     foreign key (id_endereco) references tb_enderecos (id_endereco),
-    foreign key (id_usuario) references tb_pedidos (id_Pedidos)
-);
-
-create table tb_contato (
-    id_contato serial primary key,
-    celular varchar(14) unique not null,
-    email varchar(100) unique,
 );
 
 create table tb_credenciais (
@@ -60,4 +50,16 @@ create table tb_credenciais (
 
     foreign key (id_usuario) references tb_usuarios (id_usuario),
     constraint fk_email foreign key (email) references tb_contato (email)
+);
+
+create table tb_pedidos (
+    id_pedidos serial primary key,
+    id_usuario serial unique,
+    data_pedido timestamp defaut NOW,
+    id_produtos int not null,
+    quantidade int(100),
+    preco numeric(5,2) not null
+
+    foreign key (id_usuario) references tb_usuarios(id_usuario),
+    foreign key (id_produtos) references tb_produtos(id_produtos)
 );
