bb0aa3fcf4d70354ccf69f8313ce6ea11f0fc935
Tiallysson Costa <tiallysson@proton.me>
2024-07-04 20:29:31 -0300

    Removi alguns erros de digitação e sintaxe

--- database.sql
+++ database.sql
@@ -8,28 +8,30 @@ create table tb_enderecos (
     id_endereco serial primary key,
     cep varchar(9) not null,
     rua varchar(100) not null,
-    bairro varchar(100), not null,
+    bairro varchar(100) not null,
     cidade varchar(50) not null,
     estado varchar(50) not null,
     numero varchar(10),
-    complemento varchar
+    complemento varchar(255)
 );
 
 create table tb_produtos (
     id_produtos serial primary key,
-    nome_produto varchar not null,
-    descricao varchar not null,
-    quantidade int(100),
+    nome_produto varchar(100) not null,
+    descricao varchar(255) not null,
+    quantidade int not null,
     preco numeric(5,2) not null
 );
 
 create table tb_pedidos (
     id_pedidos serial primary key,
-    id_usuario serial unique,
-    data_pedido timestamp defaut NOW,
-    produtos int not null,
-    quantidade int(100),
-    preco numeric(5,2) not null
+    id_usuario int,
+    data_pedido timestamp default NOW(),
+    id_produtos int not null,
+    quantidade int,
+    preco numeric(5,2) not null,
+
+    foreign key (id_produtos) references tb_produtos(id_produtos)
 );
 
 create table tb_usuarios (
@@ -37,26 +39,26 @@ create table tb_usuarios (
     nome varchar(50) not null,
     sobrenome varchar(50) not null,
     cpf varchar(11) unique not null,
-    data_cadastro timestamp defaut NOW,
+    data_cadastro timestamp defaut NOW(),
     id_contato int,
     id_endereco int,
 
     foreign key (id_contato) references tb_contato (id_contato),
     foreign key (id_endereco) references tb_enderecos (id_endereco),
-    foreign key (id_usuario) references tb_pedidos (id_Pedidos)
+    foreign key (id_usuario) references tb_pedidos (id_usuario)
 );
 
 create table tb_contato (
     id_contato serial primary key,
     celular varchar(14) unique not null,
-    email varchar(100) unique,
+    email varchar(100) unique
 );
 
 create table tb_credenciais (
     id_credencial serial primary key,
-    id_usuario serial unique,
+    id_usuario int unique,
     email varchar(100) not null,
-    senha varchar not null,
+    senha varchar(255) not null,
 
     foreign key (id_usuario) references tb_usuarios (id_usuario),
     constraint fk_email foreign key (email) references tb_contato (email)
