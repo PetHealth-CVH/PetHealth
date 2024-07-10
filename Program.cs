af789dde830d69572960a5a66f170d142450bdee
Tiallysson Costa <tiallysson@proton.me>
2024-07-10 19:43:50 -0300

    Adicionei linhas de código em `Program.cs`

--- Program.cs
+++ Program.cs
@@ -1,6 +1,9 @@
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapControllers();
 
 app.Run();
