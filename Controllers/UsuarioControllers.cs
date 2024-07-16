6297e84c26b7aa95896fe613d25432c3afd3e66d
Ana Julia Mantovani <anajuliamantovani001@gmail.com>
2024-07-16 19:42:38 -0300

    Adicionei as models restantes

---
+++ Controllers/UsuarioControllers.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuarioControllers : ControllerBase
+    {
+        
+    }
+}
+
+
