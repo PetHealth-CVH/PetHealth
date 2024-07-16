3f5f276e9df772cf7fa4adf6126b70e6c555e396
Ana Julia Mantovani <anajuliamantovani001@gmail.com>
2024-07-16 19:42:38 -0300

    Comecei a fazer os requests

---
+++ Models/Http_Requests/UsuariosRequest.cs
@@ -0,0 +1,27 @@
+namespace Models.HttpRequests
+{
+    public class Endereco 
+    {
+        public required string Estado {get; set;}
+        public required string Bairro {get; set;}
+        public required string Cidade {get; set;} 
+        public required string Rua {get; set;}
+        public required string Numero {get; set;}
+        public string CEP {get; set;}
+        public string Complemento {get; set;}
+    }
+    public class Credencial
+    {
+        public required string Email {get; set;}
+        public required string Senha {get; set;}
+    }
+    public class UsuarioRequest
+    {
+        public required long Cpf { get; set; }
+        public DateTime DataCadastro { get; set; }
+        public required string Nome { get; set; }
+        public required string Sobrenome { get; set; }
+        public required Credencial Credencial { get; set; }
+        public required Endereco Endereco { get; set; }
+    }
+}
